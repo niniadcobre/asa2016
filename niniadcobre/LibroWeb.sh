3c8b91aeb2c0e38341e732bab5905fa412a21144
Miriam Lechner <lechnermiriam@gmail.com>
2016-08-12 12:04:15 -0300

    una idea para arrancar

---
+++ niniadcobre/LibroWeb.sh
@@ -0,0 +1,10 @@
+#En la dirección http://pages.cs.wisc.edu/~remzi/OSTEP/ se encuentran varias versiones de un libro libre y gratuitamente disponible sobre Sistemas Operativos.
+#
+#A mitad de la página HTML se encuentra una tabla donde aparecen los URLs para descargar los más de cincuenta capítulos y apéndices del libro.
+#
+#Diseñe un script que extraiga del fuente HTML los URLs de esos capítulos, los descargue, y componga un único documento PDF completo, con esos capítulos en orden.
+#
+#Pueden ser de ayuda los utilitarios wget, pdfjoin, grep, algo de Perl. No está mal combinar sus técnicas automáticas con algo de trabajo manual (¡pero no mucho!). 
+
+wget http://pages.cs.wisc.edu/~remzi/OSTEP/ 
+grep '.pdf' index.html|sed  -e 's,^.*href=,,' -e 's, .*$,,'
