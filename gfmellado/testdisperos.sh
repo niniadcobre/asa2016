eaef24a664e0a2ac3f9294bc4588e8654e3e100a
Gustavo Mellado <gfmellado@gmail.com>
2016-09-03 09:55:38 -0300

    Versión mejorada del script para detectar archivos dispersos del ejercicio 10 del TP de Almacenamiento Parte 1

--- gfmellado/testdisperos.sh
+++ gfmellado/testdisperos.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
-archivosdispersos="Los siguientes archivos es muy probable que sean dispersos:"
+if [[ "$#" = "0" ]]
+then
+echo -e "Ejecute el script seguido del directorio que quiere analizar (ej.: $0 /tmp)"
+exit 1
+fi 
 
-for i in $( find $1 -type f -printf '%p''-''%S''\n' )
+archivosdispersos="Los siguientes archivos es muy probable que sean dispersos:"
+cantarchdisp=0
+
+for i in $( find $1 -type f -printf '%p''-''%S''\n' 2>/dev/null )
 do
 if [[ $( echo $i | cut -f 2 -d "-" | cut -f 1 -d "," ) = "0" ]]
 then
-archivodisperso=$( echo "$i" | cut -f 1 -d "-" )
+archivodisperso=${i%%-*}
+((cantarchdisp+=1))
 archivosdispersos+="\n$archivodisperso"
 fi
 done
-echo -e $archivosdispersos
 
+if [[ "$cantarchdisp" = "0" ]]
+then
+echo "No creo que existan archivos dispersos en ese directorio"
+else
+echo -e $archivosdispersos
+fi
 
