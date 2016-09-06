b1a3de2373638aecc35ded16ff0fed34623ec55d
Gustavo Mellado <gfmellado@gmail.com>
2016-09-06 00:00:37 -0300

    Tercera versión del script para testear archivos dispersos (más pulida)

--- gfmellado/testdisperos.sh
+++ gfmellado/testdisperos.sh
@@ -1,28 +1,23 @@
 #!/bin/bash
 
-if [[ "$#" = "0" ]]
-then
-echo -e "Ejecute el script seguido del directorio que quiere analizar (ej.: $0 /tmp)"
-exit 1
+if [[ $# -eq 0 ]] ; then
+	printf "Ejecute el script seguido del directorio que quiere analizar (ej.: $0 /tmp)\n"
+	exit 1
 fi 
 
 archivosdispersos="Los siguientes archivos es muy probable que sean dispersos:"
 cantarchdisp=0
 
-for i in $( find $1 -type f -printf '%p''-''%S''\n' 2>/dev/null )
-do
-if [[ $( echo $i | cut -f 2 -d "-" | cut -f 1 -d "," ) = "0" ]]
-then
-archivodisperso=${i%%-*}
-((cantarchdisp+=1))
-archivosdispersos+="\n$archivodisperso"
-fi
+for i in $( find $1 -type f -printf '%p''-''%S''\n' 2>/dev/null ) ; do
+	if [[ $( echo $i | cut -f 2 -d "-" | cut -f 1 -d "," ) = "0" ]] ; then
+		archivosdispersos+="\n${i%%-*}"
+		((cantarchdisp+=1))
+	fi
 done
 
-if [[ "$cantarchdisp" = "0" ]]
-then
-echo "No creo que existan archivos dispersos en ese directorio"
+if [[ $cantarchdisp -eq 0 ]] ; then
+	printf "No creo que existan archivos dispersos en ese directorio\n"
 else
-echo -e $archivosdispersos
+	printf "$archivosdispersos\n"
 fi
 
